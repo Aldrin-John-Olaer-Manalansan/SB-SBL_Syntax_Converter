e2365744cf0fe2341411f8e5b7965a88fb36ba12
Aldrin John Olaer Manalansan <39627255+Aldrin-John-Olaer-Manalansan@users.noreply.github.com>
2025-06-02 05:55:24 +0800

    v1.3.1
    Improved Constant Detection
    Improved Parameter Parsing
    Improved Warning Information
    Fixed some minor bugs

--- compile.cmd
+++ compile.cmd
@@ -3,16 +3,20 @@ echo off
 set appName=SB-SBL Syntax Converter
 set launcherName=Launcher
 set outRelPath=Compiled
-set compressType=2
+set compressType=0
 
 set ahkRegistryPath="HKLM\SOFTWARE\AutoHotkey"
 set ahkRegistryKey="InstallDir"
 FOR /F "tokens=2* skip=2" %%a in ('reg query %ahkRegistryPath% /v %ahkRegistryKey%') do set ahkPath=%%b
 
-mkdir "%~dp0Compiled\x86"
-mkdir "%~dp0Compiled\x64"
-
 del /s /q "%~dp0Compiled\*"
+
+if not exist "%~dp0Compiled\x86" (
+    mkdir "%~dp0Compiled\x86"
+)
+if not exist "%~dp0Compiled\x64" (
+    mkdir "%~dp0Compiled\x64"
+)
 
 "%ahkPath%\Compiler\Ahk2Exe.exe" /in "%~dp0%launcherName%.ahk2" /out "%~dp0Compiled\%launcherName%.exe" /icon "%~dp0icon.png" /base "%ahkPath%\v2\AutoHotkey32.exe" /compress %compressType%
 "%ahkPath%\Compiler\Ahk2Exe.exe" /in "%~dp0%appName%.ahk2" /out "%~dp0Compiled\x86\%appName%.exe" /icon "%~dp0icon.png" /base "%ahkPath%\v2\AutoHotkey32.exe" /compress %compressType%
